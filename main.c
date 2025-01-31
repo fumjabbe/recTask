0a22c249a33e2e836951dd84fa765ae6cccc6cad
fumjabbe <jabbelopi@gmail.com>
2025-01-31 15:45:03 +0100

    cleanup and added a main while loop that prints all temperatures

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define ADC_READ_INTERVAL 100
-static clock_t last_read_time = 0;  // Store last read time
+#define FILE_READ_END 100
+static clock_t last_temp_read_time = 0;       // Store last time temperature was read
 
 /*
 ADCvalue()
 Testfunction that reads a test ADC value from a file a location "index".
 */
-uint16_t ADCvalue(uint16_t index) {
+uint16_t ADCvalue(uint16_t index) 
+{
     FILE *file;
     uint16_t linecounter = 0;
     char buffer[32];
@@ -17,30 +20,35 @@ uint16_t ADCvalue(uint16_t index) {
 
     //open file
     file = fopen("temperature.txt", "r");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
         perror("Error opening file");
-        return 500;
+        return 0;
     }
 
-    // Read each line from the file until location is reached
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        linecounter++;
+    // Read each line from the file until location is reached or end-of-file is reached
+    while (fgets(buffer, sizeof(buffer), file) != NULL) 
+    {
+        //check if we are at the current index
         if(linecounter == index)
         {
             fclose(file);
             adcvalue = atoi(buffer);
-            if (adcvalue < 0 || adcvalue > 4095) {
+            if (adcvalue < 0 || adcvalue > 4095) 
+            {
                 printf("Error: ADC value must be between 0 and 4095.\n");
-                return 500;  // Return an error value
+                return 0;  // Return an error value
             }
             return adcvalue;
         }
+        //increase for next round
+        linecounter++;
     }
-    //index not found
-    perror("Error finding line number");
+    //file index ended
     fclose(file);
-    return 500;
+    return 0;
 }
+
 /*
 The temperature sensor reports a temperature range of -50C to +50C and can be read every 100ms.
 For example the ADC can read the following values from the sensor:
@@ -50,17 +58,25 @@ For example the ADC can read the following values from the sensor:
 */
 double getTemperature(uint16_t index)
 {
+    uint16_t readADCvalue;
     clock_t current_time = clock();
-    double elapsed_time = (double)(current_time - last_read_time);
+    double elapsed_time = (double)(current_time - last_temp_read_time);
     
-     if (elapsed_time >= ADC_READ_INTERVAL) {
-        last_read_time = current_time;  // Update last read time
-        //range of ADC with 12 bit resolution is 4095
-        return ((ADCvalue(index) / 4095.0) * 100.0) - 50.0;
-    } else {
-        printf("Waiting for next valid read...\n");
-        return 500;  // Indicate that it's too soon to read
-    }
+    if (elapsed_time >= ADC_READ_INTERVAL) 
+    {
+        last_temp_read_time = current_time;  // Update last read time
+        readADCvalue = ADCvalue(index);
+
+        if(readADCvalue == 0)
+        {
+            printf("Error: problem reading from file.\n");
+            return 0;
+        }
+        else
+        {
+            return ((readADCvalue / 4095.0) * 100.0) - 50.0;
+        }
+    } 
 }
 
 void main()
@@ -68,27 +84,14 @@ void main()
     double temp = 0.0;
     uint16_t index = 0;
 
-    struct timespec ts100 = {0, 100 * 1000000L};  // Sleep for 100ms
-    struct timespec ts50 = {0, 50 * 1000000L};  // Sleep for 100ms
-
-    nanosleep(&ts100, NULL);
-    index = 30;
-    temp = getTemperature(index);
-    printf("Temperature: %.2fC\n", temp);
-    
-    nanosleep(&ts50, NULL);
-    index = 162;
-    temp = getTemperature(index);
-    printf("Temperature: %.2fC\n", temp);
-    
-    nanosleep(&ts100, NULL);
-    index = 173;
-    temp = getTemperature(index);
-    printf("Temperature: %.2fC\n", temp);
-    
-    nanosleep(&ts100, NULL);
-    index = 137;
-    temp = getTemperature(index);
-    printf("Temperature: %.2fC\n", temp);
-
+    while (1)
+    {
+        temp = getTemperature(index);
+        if (temp)
+        {
+            index++;
+            printf("Temperature: %.2fC\n", temp);
+        }
+        if(index == 766) break;
+    } 
 }
