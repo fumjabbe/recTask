04abce855e71b8ce43c46c6e150d00c83c694d22
fumjabbe <jabbelopi@gmail.com>
2025-01-31 11:31:09 +0100

    added getTemperature function that converts ADC value to temperature.
    main just tests the outer points

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <windows.h>
+
 /*
 ADCvalue()
 Testfunction that reads a test ADC value from a file a location "index".
@@ -31,17 +33,46 @@ uint16_t ADCvalue(uint16_t index) {
     fclose(file);
     return 0;
 }
-double getTemperature()
+/*
+The temperature sensor reports a temperature range of -50C to +50C and can be read every 100ms.
+For example the ADC can read the following values from the sensor:
+
+2048 (rougly 0C)
+3000 (rougly 23C)
+*/
+double getTemperature(uint16_t index)
 {
+    uint16_t adcvalue;
+
+    adcvalue = ADCvalue(index);
+
+    Sleep(100);
+    
+    if (adcvalue < 0 || adcvalue > 4095) {
+        printf("Error: ADC value must be between 0 and 4095.\n");
+        return 0;  // Return an error value
+    }
+
+    //range of ADC with 12 bit resolution is 4095
+    return ((adcvalue / 4095.0) * 100.0) - 50.0;
 
 }
 
 void main()
 {
-    uint16_t value;
+    double temp = 0.0;
     uint16_t index = 0;
     index = 30;
-    value = ADCvalue(index);
-    printf("ok\n");
-    printf("%d",value);
+    temp = getTemperature(index);
+    printf("Temperature: %.2fC\n", temp);
+    index = 162;
+    temp = getTemperature(index);
+    printf("Temperature: %.2fC\n", temp);
+    index = 173;
+    temp = getTemperature(index);
+    printf("Temperature: %.2fC\n", temp);
+    index = 137;
+    temp = getTemperature(index);
+    printf("Temperature: %.2fC\n", temp);
+
 }
