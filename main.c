a6ecdec36bcc0dfb7edd1b6bceae175538ff1f12
fumjabbe <jabbelopi@gmail.com>
2025-02-04 13:07:49 +0100

    Squashed commit of the following:
    commit 2f4dc812736375eba4a77cd3e8f6cb6d94233bcd
    Author: fumjabbe <jabbelopi@gmail.com>
    Date:   Tue Feb 4 13:03:15 2025 +0100
    
        added binary
    
    commit 4f99353e243ecaf6394606259fc84d30b0c71c94
    Author: fumjabbe <jabbelopi@gmail.com>
    Date:   Tue Feb 4 13:02:48 2025 +0100
    
        temp returned is now a random number from the adc buffer
    
    commit d45ab2b63ea4888be2e9c761519e8549202df1e6
    Author: fumjabbe <jabbelopi@gmail.com>
    Date:   Tue Feb 4 09:45:04 2025 +0100
    
        added 500 fault handling
    
    commit 08c254ec0970cdcc202f6166fa8c6cfd505b25ce
    Author: fumjabbe <jabbelopi@gmail.com>
    Date:   Mon Feb 3 18:32:50 2025 +0100
    
        removed 100ms tick counter and used delay instead. Added HTTP post with payload history and responsecode
    
    commit 061fcf1a07ce96de60dcfc0ced88ca9fc265801d
    Author: fumjabbe <jabbelopi@gmail.com>
    Date:   Mon Feb 3 15:52:47 2025 +0100
    
        started work on the 500 failure handling
    
    commit c03aa066ece27a755dc4c93b3efbe6c291515dd3
    Author: fumjabbe <jabbelopi@gmail.com>
    Date:   Mon Feb 3 11:45:03 2025 +0100
    
        added json formatting, get time and curl post

--- main.c
+++ main.c
@@ -3,15 +3,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <curl/curl.h>
 
-#define ADC_READ_INTERVAL 100 //100ms
+#define ADC_READ_INTERVAL 100   //ms
 #define MAX_BUFFER_SIZE 1024
-#define DURATION_MS 120000 // Total duration 120000ms (2 minutes)
-#define NUM_READINGS (DURATION_MS / ADC_READ_INTERVAL)
+#define HTTPPOSTDURATION 120    //seconds
+#define SAMPLERATE 500          //ms
+#define URL "http://localhost:5000/api/temperature"
+#define ALT_URL "http://localhost:5000/api/temperature/missing"
 
-static clock_t last_time_100ms = 0;       // Store last time 100ms was reached
+static clock_t lastreadTime = 0;  // Store last read time
 static uint16_t buffer[MAX_BUFFER_SIZE];
 static uint16_t totalLinesCounter = 0;
+char payloadHistory[10][512];
+uint8_t historyIndex = 0;
+uint16_t HTTPresponseCode = 0;
 
 /*
 getTestADCvaluesFromFile()
@@ -38,15 +44,21 @@ bool getTestADCvaluesFromFile()
     fclose(file);
     return 1;
 }
+
 /*
-Function that gets the integer at a certain index
-Wraps back to start .
+Function that grabs a random adc value from test file
+For test purpose
 */
-uint16_t getADCvalue(uint16_t index)
+uint16_t getADCvalue()
 {
-    uint16_t targetIndex = (index - 1) % totalLinesCounter + 1;
-    return buffer[targetIndex];
+    //ensure different outputs on each run.
+    srand(time(0));
+    //ensure random number is in range of total lines in file
+    int randomNumber = rand() % totalLinesCounter;
+
+    return buffer[randomNumber];
 }
+
 /*
 The temperature sensor reports a temperature range of -50C to +50C and can be read every 100ms.
 For example the ADC can read the following values from the sensor:
@@ -54,21 +66,131 @@ For example the ADC can read the following values from the sensor:
 2048 (rougly 0C)
 3000 (rougly 23C)
 */
-double getTemperature(uint16_t index)
+double getTemperature()
 {
-    return ((getADCvalue(index) / 4095.0) * 100.0) - 50.0;
-}
-/*
-Function that is true each time 100ms clock cycles has passed
-*/
-bool tasks100ms(void)
-{
-    //get current clock cycles since program start
-    clock_t current_time = clock();
-    double elapsed_time = (double)(current_time - last_time_100ms);
+    clock_t currentTime = clock();
+    double elapsed_time = (double)(currentTime - lastreadTime);
     if (elapsed_time >= ADC_READ_INTERVAL) 
     {
-        last_time_100ms = current_time;  // Update last read time
+        lastreadTime = currentTime;  // Update last read time
+        return ((getADCvalue() / 4095.0) * 100.0) - 50.0;
+    }
+    else 
+    {
+        printf("Waiting for next valid read...\n");
+        return -100;  // Indicate that it's too soon to read
+    }
+}
+
+/*
+Function to get current time.
+Returns time in buffer formatted in ISO8601 format
+*/
+void getTime(char *buffer, size_t size) 
+{
+    time_t now = time(NULL);
+    struct tm *tm_info = gmtime(&now);
+    strftime(buffer, size, "%Y-%m-%dT%H:%M:%SZ", tm_info);
+}
+uint16_t postHTTP(const char *url, const char *payload)
+{
+    CURL *curl;
+    CURLcode res;
+    long responseCode = 0;
+
+    //post the data
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    
+    if (curl) 
+    {
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload);
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+        
+        //get response code
+        if(res == CURLE_OK)
+        {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &responseCode);            
+        }
+        //error code from example
+        else
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        // Cleanup
+        curl_slist_free_all(headers);
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+
+    return responseCode;
+}
+/*
+Function that formats JSON payload and post to HTTP rest api
+*/
+void formatJSONPostHTTP(char *startTime, char *endTime, double minTemp, double maxTemp, double avgTemp)
+{
+    char payload[512];
+    
+    // Format the JSON payload
+    snprintf(payload, sizeof(payload), 
+             "{\"time\": {\"start\": \"%s\", \"end\": \"%s\"}, \"min\": %.2f, \"max\": %.2f, \"Avg\": %.2f}", 
+             startTime, endTime, minTemp, maxTemp, avgTemp);
+
+    //store it, up to 10 last payloads
+    if(historyIndex >= 10) historyIndex = 0;
+
+    strcpy(payloadHistory[historyIndex], payload);
+    
+    //if no failure has been registered, attempt sending it
+    if(HTTPresponseCode != 500)
+    {
+        HTTPresponseCode = postHTTP(URL, payload);
+        historyIndex++;
+        return;
+    }
+    
+    //last attempt failed, send history to alternative url and the last history entry to current url
+    if(HTTPresponseCode == 500)
+    { 
+        char jsonArrayPayload[1024] = "[";
+        for (int i = 0; i < historyIndex; i++) 
+        {
+            strcat(jsonArrayPayload, payloadHistory[historyIndex]);
+            strcat(jsonArrayPayload, ",");
+        }
+        strcat(jsonArrayPayload, "]");
+
+        //send the last 10 stored payloads to alternative url
+        HTTPresponseCode = postHTTP(ALT_URL, jsonArrayPayload);
+        //send the last payload history entry
+        HTTPresponseCode = postHTTP(URL, payloadHistory[historyIndex-1]);
+        //also send the current payload
+        HTTPresponseCode = postHTTP(URL, payload);
+        historyIndex++;
+    }
+}
+
+/*
+Check each time http post duration has passed
+*/
+bool taskpostHttp(void)
+{
+    static time_t last_call_time = 0;
+    time_t current_time = time(NULL);
+
+    if (current_time - last_call_time >= HTTPPOSTDURATION) 
+    {
+        last_call_time = current_time;  // Update last read time
         return true;
     }
     return false;
@@ -80,16 +202,24 @@ void main()
             minVal = 0.0,
             maxVal = 0.0,
             avgVal = 0.0;
-    uint16_t reading = 0;
+    //loopcounter used for avg reading
+    uint16_t loopcounter = 0;   
+
+    char startTime[32];
+    char endTime[32];
+
+    taskpostHttp();                                         //init time
 
     if(getTestADCvaluesFromFile())                          //read file and store content to memory
-    {                                                       //only start program if file is found
+    {
+        system("echo recTask started");
+        getTime(startTime, sizeof(startTime));              //get the time at the start of the program.
         while (1)
         {
-            if (tasks100ms)
-            {
-                temperature = getTemperature(reading);      //Get a new reading each 100ms
-                reading++;
+            temperature = getTemperature();                 //Get a new reading
+            if(temperature != -100)
+            {   
+                loopcounter++;
                 
                 if (temperature < minVal) 
                 {
@@ -100,14 +230,21 @@ void main()
                     maxVal = temperature;                   //Store the highest temperature registered in this periode
                 }
                 avgVal += temperature;
+            }
 
-                if(reading >= NUM_READINGS)                 //Each 2.minute, calculate average and post to rest api
-                {
-                    avgVal = avgVal / NUM_READINGS;
-                    
-                    minVal = 0.0;
-                    maxVal = 0.0;
-                }
+            Sleep(SAMPLERATE);
+            
+            if(taskpostHttp())                                  //Each 2.minute, calculate average and post to HTTP REST endpoint
+            {
+                avgVal = avgVal / loopcounter;
+                getTime(endTime, sizeof(endTime));          //get the time each 2minute interval as endtime variable.
+                formatJSONPostHTTP(startTime, endTime, minVal, maxVal, avgVal);
+                 
+                //reset interval variables
+                loopcounter = 0;
+                minVal = 0.0;                           
+                maxVal = 0.0;
+                getTime(startTime, sizeof(startTime));      //get new start time for next interval
             }
         }
     } 
